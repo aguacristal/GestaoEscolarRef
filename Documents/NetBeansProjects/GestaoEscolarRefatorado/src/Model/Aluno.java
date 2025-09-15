8e76f0f1a8bd3db45ed2b83d197b5abb13dd7636
aguacristal <claracristine19@gmail.com>
2025-09-15 20:29:47 -0300

    Classe aluno

---
+++ Documents/NetBeansProjects/GestaoEscolarRefatorado/src/Model/Aluno.java
@@ -0,0 +1,47 @@
+
+package Model;
+
+/**
+ *
+ * @author Clara
+ */
+public class Aluno {
+     private int id;
+    private String matricula;
+    private String turma;
+    private Responsavel responsavel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public Responsavel getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(Responsavel responsavel) {
+        this.responsavel = responsavel;
+    }
+}
+}
